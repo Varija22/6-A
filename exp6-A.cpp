a5337cdb75410fd4d567cb02904923b51f67fd0a
Varija22 <varija.korti.btech2024@sitpune.edu.in>
2025-07-28 14:56:04 +0530

    Add files via upload

---
+++ exp6-A.cpp
@@ -0,0 +1,14 @@
+//Varija K
+//24070123165
+
+//Program 1
+#include <iostream> 
+using namespace std; 
+
+int main() {
+    for (int i = 0; i <= 10; i += 2) {
+        cout << i << endl;
+    }
+    return 0;
+}
+
