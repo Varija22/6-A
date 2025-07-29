93820835edcbb70130bc862c88a3e3231c80b721
Varija22 <varija.korti.btech2024@sitpune.edu.in>
2025-07-29 15:06:40 +0530

    Add files via upload

---
+++ exp6-B.cpp
@@ -0,0 +1,14 @@
+//Varija K
+//24070123165
+
+//Program 2
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 0; i < 5; i++) {
+        cout << "SIT" << endl;
+    }
+    return 0;
+}
+
